dcdb29c462d0cb8225c5a1dcba9f33e31f89b7ef
ali4210 <saleemali.mohammad@gmail.com>
2025-12-09 13:09:26 +0600

    Mission Accomplished: Full Windows & Linux Automation

--- modules/profile/manifests/db.pp
+++ modules/profile/manifests/db.pp
@@ -1,17 +1,14 @@
 class profile::db {
 
+  # --- Linux Logic (Keep this as is) ---
   if $facts['kernel'] == 'Linux' {
-    class { 'mysql::client':
-      package_name => 'mariadb-client',
-    }
-
+    class { 'mysql::client': package_name => 'mariadb-client' }
     class { 'mysql::server':
       package_name            => 'mariadb-server',
       service_name            => 'mariadb',
       root_password           => lookup('system_setup::db_root_password'),
       remove_default_accounts => true,
     }
-
     mysql::db { 'puppet_db':
       user     => lookup('system_setup::db_user'),
       password => lookup('system_setup::db_user_password'),
@@ -20,4 +17,19 @@ class profile::db {
     }
   }
 
+# --- Windows Logic ---
+  if $facts['kernel'] == 'windows' {
+    
+    include chocolatey
+
+    package { 'mariadb':
+      ensure          => installed,
+      provider        => 'chocolatey',
+      # THIS LINE IS CRITICAL FOR SECURITY:
+      install_options => ['--install-arguments', "\"PASSWORD=${lookup('system_setup::db_root_password')}\""],
+      require         => Class['chocolatey'], 
+    }
+  }
 }
+
+
